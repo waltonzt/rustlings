dbc35659c8b2bf7e4e38b60b4fe14672b77fc6df
waltonzt <ztwalto@gmail.com>
2024-05-20 10:27:25 -0400

    Progress

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -3,10 +3,9 @@
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
+    type int = u8;
+    let x: int = 0;
     if x == 10 {
         println!("x is ten!");
     } else {
